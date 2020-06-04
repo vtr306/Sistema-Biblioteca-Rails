56e627e9b487be6209d9bc030302468eb8c7f7e0
Vitor Rosa <vitorosa306@gmail.com>
2020-06-04 16:33:07 -0300

    Created the seed to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+    Author.create!(name: Faker::Book.unique.author)
+    Category.create!(name: Faker::Book.unique.genre)
+    Client.create!(name: Faker::Name.unique.name)
+    Librarian.create!(email: Faker::Internet.unique.email)
+end
+
+5.times do
+    Book.create!(name: Faker::Book.unique.title, stock: Faker::Number.unique.between(from: 0, to: 10), author: Author.all.sample, category: Category.all.sample)
+end
+
+3.times do
+    Reservation.create!(book: Book.all.sample, client: Client.all.sample, librarian: Librarian.all.sample)
+end
