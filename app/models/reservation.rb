985083dc2850daeea78d6251f561b1eb159a58e5
Vitor Rosa <vitorosa306@gmail.com>
2020-06-07 17:49:16 -0300

    Added callback that reduces in one the stock after a reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,11 @@ class Reservation < ApplicationRecord
   belongs_to :book
   belongs_to :client
   belongs_to :librarian
+
+  after_create :atualize_stock
+
+  private
+    def atualize_stock
+      self.book.update(stock: book.stock-1)
+    end
 end
